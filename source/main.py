0a601a030351bb4f54fa2bfd87cd6636cd55b31d
Mohammad Zarif <m.zarif1996@gmail.com>
2025-01-26 09:16:27 +0330

    add qc checker class

---
+++ source/main.py
@@ -0,0 +1,35 @@
+import json
+import logging
+from datetime import datetime, timedelta, time
+from pathlib import Path
+import numpy as np
+import pandas as pd
+from tkinter import filedialog
+from pandas.core.tools import datetimes
+from typing import Optional, List, Dict, Iterable, Tuple, Any
+
+from source.common.main_class import MainClass
+
+
+class QCChecker(MainClass):
+    """
+    A class to analyze trade spreads, check stop loss hits, and generate trade reports.
+    """
+
+    def __init__(self, base_path: Path, debug: bool = False) -> None:
+        """
+        Initialize the TradeSpreadAnalyze instance.
+
+        :param base_path: Base directory path.
+        :param debug: Flag to enable debug mode.
+        """
+        super().__init__(base_path)
+        self.debug: bool = debug
+        self.result_path = self.base_path / "results"
+        self.result_path.mkdir(parents=True, exist_ok=True)
+
+    def __run__(self):
+        """Initialize data by loading symbol, spreads, candles, and report."""
+        print('Data Analysis complete.')
+
+
