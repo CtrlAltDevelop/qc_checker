24246e82d7fcd78678db6c3834dfc50bf3b07562
Mohammad Zarif <m.zarif1996@gmail.com>
2025-01-27 13:55:45 +0330

    clean code and run for mt5

--- source/main.py
+++ source/main.py
@@ -91,13 +91,23 @@ class QCChecker(MainClass):
                 continue
 
             problem = self._multi_file_analysis(symbol_path, data_frames)
+            if problem:
+                continue
+
             break
 
-        # time.sleep(.1)
-        # for symbol_path in tqdm([d for d in mt5_folder.iterdir() if d.is_dir()], desc=f"MT 5: {mt4_folder.name}"):
-        #     problem, data_frames = self._read_symbol_folder_data(symbol_path, [
-        #         'time', 'open', 'high', 'low', 'close', 'tick volume', 'volume', 'spread'])
-        #     break
+        time.sleep(.1)
+        for symbol_path in tqdm([d for d in mt5_folder.iterdir() if d.is_dir()], desc=f"MT 5: {mt4_folder.name}"):
+            problem, data_frames = self._read_symbol_folder_data(symbol_path, [
+                'time', 'open', 'high', 'low', 'close', 'tick volume', 'volume', 'spread'])
+            if problem:
+                continue
+
+            problem = self._multi_file_analysis(symbol_path, data_frames)
+            if problem:
+                continue
+
+            break
 
         time.sleep(.1)
         logging.info('Data Analysis complete.')
@@ -154,14 +164,14 @@ class QCChecker(MainClass):
                 invalid_indices.append(idx)
         return invalid_indices
 
-    def _single_file_analysis(self, candle_path: Path, df: pd.DataFrame, columns: List[str]) -> Tuple[bool, pd.DataFrame]:
+    def _single_file_analysis(self, path: Path, df: pd.DataFrame, columns: List[str]) -> Tuple[bool, pd.DataFrame]:
         error = False
         df = df.copy()
         # Validate the header of the DataFrame
         try:
             df.columns = columns
         except ValueError as e:
-            self.red_flags[candle_path].append(f"Missing required columns: {e}")
+            self.red_flags[path].append(f"Missing required columns: {e}")
             error = True
 
         # Validate the time column for correct date format.
@@ -170,52 +180,52 @@ class QCChecker(MainClass):
                 df['time'] = pd.to_datetime(df['time'], format="%d.%m.%Y %H:%M:%S.%f", dayfirst=True)
             except ValueError:
                 invalid_indices = self.find_invalid_time_rows(df['time'])
-                self.red_flags[candle_path].append(f"Rows with invalid time format: {invalid_indices}")
+                self.red_flags[path].append(f"Rows with invalid time format: {invalid_indices}")
                 error = True
 
         # Find the indices of empty rows
         if not error:
             empty_row_indices = df[df.isnull().all(axis=1)].index
             if not empty_row_indices.empty:
-                self.red_flags[candle_path].append(f"Indices of empty rows: {list(empty_row_indices)}")
+                self.red_flags[path].append(f"Indices of empty rows: {list(empty_row_indices)}")
                 error = True
 
         # Find the indices of empty columns
         if not error:
             empty_column_indices = df.columns[df.isnull().all()]
             if len(empty_column_indices) > 0:
-                self.red_flags[candle_path].append(f"Indices of empty columns: {list(empty_column_indices)}")
+                self.red_flags[path].append(f"Indices of empty columns: {list(empty_column_indices)}")
                 error = True
 
         # Find the indices of duplicate rows
         if not error:
             duplicate_indices = df[df.duplicated()].index
             if not duplicate_indices.empty:
-                self.red_flags[candle_path].append(f"Indices of duplicate rows: {list(duplicate_indices)}")
+                self.red_flags[path].append(f"Indices of duplicate rows: {list(duplicate_indices)}")
                 error = True
 
         # Check High and Low value
         high_is_max = (df['high'] != df[['open', 'high', 'low', 'close']].max(axis=1))
         low_is_min = (df['low'] != df[['open', 'high', 'low', 'close']].min(axis=1))
         for idx in sorted(set(high_is_max[high_is_max].index) | set(low_is_min[low_is_min].index)):
-            self.red_flags[candle_path].append(f"Wrong Candle Data in Index {idx}")
+            self.red_flags[path].append(f"Wrong Candle Data in Index {idx}")
             error = True
         return error, df.sort_values(by='time')
 
-    def _multi_file_analysis(self, candle_path: Path, dfs: Dict[int, pd.DataFrame]) -> bool:
+    def _multi_file_analysis(self, path: Path, dfs: Dict[int, pd.DataFrame]) -> bool:
         error = False
 
         # Check start times
         start_times = [(i, df['time'].iloc[0].date()) for i, df in dfs.items()]
         if len(set(t for _, t in start_times)) != 1:
             error = True
-            self.red_flags[candle_path].append(
+            self.red_flags[path].append(
                 f"TimeFrames Data do not start at the same datetime. Details: {start_times}")
 
         # Check end times
         end_times = [(i, df['time'].iloc[-1].date()) for i, df in dfs.items()]
         if len(set(t for _, t in end_times)) != 1:
             error = True
-            self.red_flags[candle_path].append(
+            self.red_flags[path].append(
                 f"TimeFrames Data do not end at the same datetime. Details: {end_times}")
         return error
